ff75ce1412da8e190dfe6caa9d1cc3d55974f21c
nicocugno <nicocugno2@gmail.com>
2023-09-20 23:35:52 -0300

    Creacion de boton y fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export const App = () => {
+
+  const urlBase = 'https://api.openweathermap.org/data/3.0/weather'
+  const API_KEY = '892124d384be1528bb15466e8aec5c5c'
+
+
+  const [ciudad, setCiudad] = useState('')
+  const [dataClima, setdataClima] = useState(null)
+
+
+  const handleCambioCiudad = (e) => {
+    setCiudad(e.target.value);
+  }
+
+  const handleSubmit = (e) => { 
+    e.preventDefault();
+    if(ciudad.length > 0) fetchClima();
+  }
+
+
+  const fetchClima = async () => {
+    try {
+      const response = await fetch(`${urlBase}?=${ciudad}&appid=${API_KEY}`)
+      const data = await response.json()
+      setdataClima(data)
+    }catch{
+        console.error('Ocurrio el siguiente problema: ', error)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='container'>
 
-export default App
+      <h1>Aplicacion del Clima</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" 
+        value={ciudad}
+        onChange = {handleCambioCiudad} 
+        />
+        <button type='submit'>Buscar</button>
+      </form>
+
+    </div>
+  )
+  }
+
